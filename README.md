f4c5386469c5c5d22999f342612674829f0048ef
chaeyeong <cherry12@sk.com>
2022-09-28 13:17:18 +0900

    feature: add basic logic

--- README.md
+++ README.md
@@ -1,46 +1,11 @@
-# Getting Started with Create React App
+### React 기본 개념을 위한 Project
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## 1. useEffect를 이용한 변수
+- useEffect를 이용하여 state 변수 변경
 
-## Available Scripts
+## 2. clickOutofSide Hook 만들기
+- hook으로 외부 클릭하면 닫히는 hook 만들기 
 
-In the project directory, you can run:
 
-### `yarn start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `yarn test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+## 3. render 최적화 
+- 데이터가 변경되더라고 관련 없는 Child Component들은 rendering이 안 되게 최적화
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import './assets/css/front.css';
+
+export const friends = [
+    { id: 'GENERAL', label: '이곳을 자주 방문하는 친구들에게' },
+    { id: 'REALTIME', label: '지금 이곳 주변에 있는 친구들에게' },
+];
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [target, setTarget] = useState<{id: string; label: string;}>(friends[0]);
+    return (
+        <div className="qfeed-wrap">
+            <div className="page-question">
+                <div className="container-top">
+                    <div className="txt-title">Test</div>
+                </div>
+                <div className="container-content">
+                    <div className="content-area type-location">
+                        <h2 className="txt-title">대상</h2>
+                        <div className="setting-block">
+                            <div className="component-dropdown">
+                                {
+                                    // 1. target State를 이용해 동적 셋팅
+                                }
+                                <span className="txt-label">이곳을 자주 방문하는 친구들에게</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="content-area">
+                        <h2 className="txt-title">대상 선택</h2>
+                        <div className="setting-block">
+
+                            {
+                                // 2. 클릭 시, drop-down list show/hide
+                                // drop-down hide: aria-expanded="false"
+                                // drop-down show : aira-expanded="true"
+                            }
+                            <div className="component-dropdown" aria-expanded="true">
+                                <span className="txt-label">이곳을 자주 방문하는 친구들에게</span>
+                                <div className="layer-dropdown">
+                                    <div className="box-dropdown">
+                                        <ul className="list-options">
+                                        {
+                                            friends.map((friend) =>
+                                                <li key={friend.id} >
+                                                    {
+                                                        // 3. dropdown이 open 될 때, 선택되어 있는 값 셋팅
+                                                        // checked="true" 이면 리스트 선택 됨.
+                                                    }
+                                                    <input type="radio" id={friend.id} name={friend.id} checked/>
+                                                    <label className="item-option" htmlFor={friend.id}>
+                                                        <span className="txt-option">{friend.label}</span>
+                                                    </label>
+                                                </li>
+                                           )
+                                        }
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="section-button">
+                    <button type="button" className="btn-extension">초기화</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
